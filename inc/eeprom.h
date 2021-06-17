674ac51f966bf53b3accee8d84368e9f03d178f4
Rachel <12876215@student.uts.edu.au>
2021-06-17 11:16:55 +1000

    Setting up EEPROM rip back to i2c

---
+++ inc/eeprom.h
@@ -0,0 +1,15 @@
+/*
+* EEPROM 
+* Medium and medium+ density STM8L15x microcontrollers feature from16 to 32 Kbytes of
+* Flash memory, plus 1 Kbyte of data EEPROM
+*/
+
+#ifndef EEPROM_H
+#define EEPROM_H
+
+#include <stdio.h>
+
+
+
+
+#endif /* EEPROM_H */
